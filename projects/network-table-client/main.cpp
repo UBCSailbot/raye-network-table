1c502ea4b5c1c449f3f76bae82499c1a0b81a654
Alexmac22347 <alexmac22347@gmail.com>
2017-08-20 12:36:58 -0700

    Minor changes to network-table-client The server is now queried multiple times in a loop

--- projects/network-table-client/main.cpp
+++ projects/network-table-client/main.cpp
@@ -22,46 +22,50 @@
  * table is returned.
  */
 int main() {
+    int num_queries = 10;
     int num_errors = 0;
 
     NetworkTable::Connection connection;
 
-    // SET windspeed to 100
-    try {
-        NetworkTable::Value *value = new NetworkTable::Value();
-        value->set_type(NetworkTable::Value::INT);
-        value->set_int_data(100);
+    for (int i = 0; i < num_queries; i++) {
+        // SET windspeed to 100
+        try {
+            NetworkTable::Value *value = new NetworkTable::Value();
+            value->set_type(NetworkTable::Value::INT);
+            value->set_int_data(100);
 
-        NetworkTable::SetValueRequest *setvalue_request = new NetworkTable::SetValueRequest();
-        setvalue_request->set_key("windspeed");
-        setvalue_request->set_allocated_value(value);
+            NetworkTable::SetValueRequest *setvalue_request = new NetworkTable::SetValueRequest();
+            setvalue_request->set_key("windspeed");
+            setvalue_request->set_allocated_value(value);
 
-        NetworkTable::Request request;
-        request.set_type(NetworkTable::Request::SETVALUE);
-        request.set_allocated_setvalue_request(setvalue_request);
+            NetworkTable::Request request;
+            request.set_type(NetworkTable::Request::SETVALUE);
+            request.set_allocated_setvalue_request(setvalue_request);
 
-        connection.Send(request);
-    } catch (...) {
-        num_errors++;
-    }
-    // GET windspeed
-    try {
-        NetworkTable::GetValueRequest *getvalue_request = new NetworkTable::GetValueRequest();
-        getvalue_request->set_key("windspeed");
+            connection.Send(request);
+        } catch (...) {
+            num_errors++;
+        }
+        // GET windspeed
+        try {
+            NetworkTable::GetValueRequest *getvalue_request = new NetworkTable::GetValueRequest();
+            getvalue_request->set_key("windspeed");
 
-        NetworkTable::Request request;
-        request.set_type(NetworkTable::Request::GETVALUE);
-        request.set_allocated_getvalue_request(getvalue_request);
+            NetworkTable::Request request;
+            request.set_type(NetworkTable::Request::GETVALUE);
+            request.set_allocated_getvalue_request(getvalue_request);
 
-        connection.Send(request);
+            connection.Send(request);
 
-        NetworkTable::Reply reply;
-        connection.Receive(&reply);
-        std::cout << "received reply: windspeed = " \
-                  << reply.getvalue_reply().value().int_data() \
-                  << std::endl;
-    } catch (...) {
-        num_errors++;
+            NetworkTable::Reply reply;
+            connection.Receive(&reply);
+
+            // There's no point in checking the value of the reply
+            // because there is no garuantee that is will be the same
+            // value as when we last set it.
+        } catch (...) {
+            num_errors++;
+        }
     }
 
     return num_errors;
