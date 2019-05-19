1fce1db5471d171119da830aa7f2a7954883e9aa
Alex Macdonald <alexmac22347@gmail.com>
2019-05-18 17:43:23 -0700

    Change structure of network table created by client so it looks more like what the web app people are expecting

--- projects/client/main.cpp
+++ projects/client/main.cpp
@@ -14,32 +14,31 @@
 #include <map>
 #include <string>
 
-std::atomic_bool winddirectioncallback_called(false); // It would be better to
+std::atomic_bool sensor_a_callback_called(false); // It would be better to
                                                       // check that it was called
                                                       // the correct number of times
                                                       // but this depends on what other
                                                       // clients are doing.
-std::atomic_bool wrong_wind_data_received(false);
-void WindDirectionCallback(NetworkTable::Node node) {
-    winddirectioncallback_called = true;
-    if (node.value().int_data() != 20 && node.value().int_data() != 40) {
-        std::cout << "Received wind direction data:" << node.value().int_data() << std::endl;
-        std::cout << "Expected: " << 20 << " or " << 40 << std::endl;
-        wrong_wind_data_received = true;
+std::atomic_bool wrong_sensor_a_data_received(false);
+void SensorACallback(NetworkTable::Node node) {
+    sensor_a_callback_called = true;
+    if (node.children().at("a").children().at("b").value().int_data() != 20) {
+        std::cout << "Received sensor a data: "\
+            << node.children().at("a").children().at("b").value().int_data() << std::endl;
+        std::cout << "Expected: " << 20 << std::endl;
+        wrong_sensor_a_data_received = true;
     }
 }
 
-std::atomic_bool batterycallback_correct_data(false);
-std::atomic_bool batterycallback_called(false);
-void BatteryCallback(NetworkTable::Node node) {
-    batterycallback_called = true;
-    int charge = node.children().at("BAT0").children().at("charge").value().int_data();
-    bool is_charging =  node.children().at("BAT0").children().at("is_charging").value().bool_data();
-    if (is_charging == true && charge == 63) {
-        batterycallback_correct_data = true;
+std::atomic_bool sensor_b_callback_correct_data(false);
+std::atomic_bool sensor_b_callback_called(false);
+void SensorBCallback(NetworkTable::Node node) {
+    sensor_b_callback_called = true;
+    int a = node.children().at("a").value().int_data();
+    if (a == 63) {
+        sensor_b_callback_correct_data = true;
     } else {
-        std::cout << "Received wrong battery data. charge: " << charge << std::endl\
-            << "is charging: " << is_charging << std::endl;
+        std::cout << "Received sensor b data: " << a << std::endl;
     }
 }
 
@@ -73,20 +72,19 @@ int main() {
         return 0;
     }
 
-    // Subscribe to wind direction.
     // Note that even though we subscribe to badcallback first,
-    // we override it with a call to winddirectioncallback.
-    connection.Subscribe("winddirection", &BadCallback);
-    connection.Subscribe("winddirection", &WindDirectionCallback);
+    // we override it with sensoracallback.
+    connection.Subscribe("sensor_a", &BadCallback);
+    connection.Subscribe("sensor_a", &SensorACallback);
 
-    // Subscribe to windspeed then immediately unsubscribe.
+    // Subscribe then immediately unsubscribe.
     connection.Subscribe("bad", &BadCallback);
     connection.Unsubscribe("bad");
 
     // Subscribe to something shallow.
     // Then, when something deep in the tree gets updated, we should still
     // receive the update
-    connection.Subscribe("batteries", &BatteryCallback);
+    connection.Subscribe("sensor_b", &SensorBCallback);
  
     // Without this timeout, it is possible for BadCallback to be called.
     // What happens in between the call to subscribe with BadCallback, and
@@ -94,60 +92,53 @@ int main() {
     // of windspeed or winddirection, causing BadCallback to get called.
     std::this_thread::sleep_for(std::chrono::seconds(3));
 
-    // SET battery info
+    // SET
     try {
-        NetworkTable::Value charge;
-        charge.set_type(NetworkTable::Value::INT);
-        charge.set_int_data(63);
-        NetworkTable::Value is_charging;
-        is_charging.set_type(NetworkTable::Value::BOOL);
-        is_charging.set_bool_data(true);
+        NetworkTable::Value value;
+        value.set_type(NetworkTable::Value::INT);
+        value.set_int_data(20);
 
         std::map<std::string, NetworkTable::Value> values;
-        values.insert(std::pair<std::string, NetworkTable::Value>("batteries/BAT0/charge", charge));
-        values.insert(std::pair<std::string, NetworkTable::Value>("batteries/BAT0/is_charging", is_charging));
+        values.insert(std::pair<std::string, NetworkTable::Value>("sensor_a/a/b", value));
         connection.SetValues(values);
     } catch (...) {
         std::cout << "Error setting battery info" << std::endl;
         any_test_failed = 1;
     }
 
-    // SET wind direction
+    // SET
     try {
         NetworkTable::Value value;
         value.set_type(NetworkTable::Value::INT);
-        value.set_int_data(20);
-        connection.SetValue("winddirection", value);
+        value.set_int_data(63);
+        connection.SetValue("sensor_b/a", value);
     } catch (...) {
         std::cout << "Error setting wind direction" << std::endl;
         any_test_failed = 1;
     }
 
-    // SET windspeed
+    // SET
     try {
         NetworkTable::Value value;
         value.set_type(NetworkTable::Value::INT);
         value.set_int_data(100);
-        connection.SetValue("windspeed", value);
+        connection.SetValue("sensor_c/a", value);
     } catch (...) {
-        std::cout << "Error setting wind speed" << std::endl;
+        std::cout << "Error setting sensor_c/a" << std::endl;
         any_test_failed = 1;
     }
 
     for (int i = 0; i < num_queries; i++) {
-        // GET windspeed
+        // GET 
         try {
-            NetworkTable::Value value = connection.GetValue("windspeed");
-            // In this case I know that the value of the windspeed should be.
-            // Normally it is possible for other processes to be modifying
-            // the data, so there is no way to know what it should be.
+            NetworkTable::Value value = connection.GetValue("sensor_c/a");
             if (value.int_data() != 100) {
-                std::cout << "Wrong wind speed data" << std::endl;
+                std::cout << "Wrong sensor_c/a data" << std::endl;
                 any_test_failed = 1;
             }
         } catch (NetworkTable::TimeoutException) {
         } catch (...) {
-            std::cout << "Error getting wind speed" << std::endl;
+            std::cout << "Error getting sensor_c/a" << std::endl;
             any_test_failed = 1;
         }
         // GET garbage
@@ -162,47 +153,47 @@ int main() {
             std::cout << "Error getting garbage" << std::endl;
             any_test_failed = 1;
         }
-        // SET latitude and longitude
-        NetworkTable::Value latitude;
-        latitude.set_type(NetworkTable::Value::DOUBLE);
-        latitude.set_double_data(60.3225);
-        NetworkTable::Value longitude;
-        longitude.set_type(NetworkTable::Value::DOUBLE);
-        longitude.set_double_data(155.9594);
+        // SET
+        NetworkTable::Value val_a;
+        val_a.set_type(NetworkTable::Value::DOUBLE);
+        val_a.set_double_data(60.3225);
+        NetworkTable::Value val_b;
+        val_b.set_type(NetworkTable::Value::DOUBLE);
+        val_b.set_double_data(155.9594);
         try {
             std::map<std::string, NetworkTable::Value> values;
-            values.insert(std::pair<std::string, NetworkTable::Value>("gps/latitude", latitude));
-            values.insert(std::pair<std::string, NetworkTable::Value>("gps/longitude", longitude));
+            values.insert(std::pair<std::string, NetworkTable::Value>("sensor_d/a", val_a));
+            values.insert(std::pair<std::string, NetworkTable::Value>("sensor_d/b", val_b));
 
             connection.SetValues(values);
         } catch (...) {
-            std::cout << "Error setting latitude and longitude" << std::endl;
+            std::cout << "Error setting sensor_d" << std::endl;
             any_test_failed = 1;
         }
         // GET latitude and longitude
         try {
             std::set<std::string> keys;
-            keys.insert("gps/latitude");
-            keys.insert("gps/longitude");
+            keys.insert("sensor_d/a");
+            keys.insert("sensor_d/b");
             
             auto values = connection.GetValues(keys);
-            if (!(std::abs(values["gps/latitude"].double_data() - latitude.double_data()) < precision)) {
-                std::cout << "Error, wrong value for gps/latitude" << std::endl;
+            if (!(std::abs(values["sensor_d/a"].double_data() - val_a.double_data()) < precision)) {
+                std::cout << "Error, wrong value for sensor_d/a" << std::endl;
                 any_test_failed = 1;
             }
-            if (!(std::abs(values["gps/longitude"].double_data() - longitude.double_data()) < precision)) {
-                std::cout << "Error, wrong value for gps/longitude" << std::endl;
+            if (!(std::abs(values["sensor_d/b"].double_data() - val_b.double_data()) < precision)) {
+                std::cout << "Error, wrong value for sensor_d/b" << std::endl;
                 any_test_failed = 1;
             }
         } catch (NetworkTable::TimeoutException) {
         } catch (...) {
-            std::cout << "Error getting gps/latitude and gps/longitude" << std::endl;
+            std::cout << "Error getting sensor_d" << std::endl;
             any_test_failed = 1;
         }
         // GET the whole tree
         try {
             auto root = connection.GetNode("/");
-            if (!(std::abs(root.children().at("gps").children().at("latitude").value().double_data() - latitude.double_data()) < precision)) {
+            if (!(std::abs(root.children().at("sensor_d").children().at("a").value().double_data() - val_a.double_data()) < precision)) {
                 std::cout << "Error, gps/latitude was wrong when getting whole tree" << std::endl;
                 any_test_failed = 1;
             }
@@ -214,20 +205,20 @@ int main() {
     }
 
     // Check that everything went OK with the callbacks
-    if (!winddirectioncallback_called) {
-        std::cout << "Error, wind direction callback was never called" << std::endl;
+    if (!sensor_a_callback_called) {
+        std::cout << "Error, sensor_a_callback was never called" << std::endl;
         any_test_failed = 1;
     }
-    if (wrong_wind_data_received) {
-        std::cout << "Wrong wind data received" << std::endl;
+    if (wrong_sensor_a_data_received) {
+        std::cout << "Wrong sensor a data received" << std::endl;
         any_test_failed = 1;
     }
-    if (!batterycallback_called) {
-        std::cout << "Error, battery callback was never called" << std::endl;
+    if (!sensor_b_callback_called) {
+        std::cout << "Error, sensor_b_callback was never called" << std::endl;
         any_test_failed = 1;
     }
-    if (!batterycallback_correct_data) {
-        std::cout << "Error, battery callback received the wrong data" << std::endl;
+    if (!sensor_b_callback_correct_data) {
+        std::cout << "Error, sensor_b_callback received the wrong data" << std::endl;
         any_test_failed = 1;
     }
     if (badcallback_called) {
