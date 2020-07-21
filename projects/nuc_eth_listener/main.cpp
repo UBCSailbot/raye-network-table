adf3bd83cf0d19d5fa778c167d3b292ab1a94c16
jahn18 <john.ahn.es@gmail.com>
2020-07-20 19:01:16 -0700

    NAV-48: Added all necessary sensor data to nuc_eth_listener/main.cpp under PublishSensorData() and published it.

--- projects/nuc_eth_listener/main.cpp
+++ projects/nuc_eth_listener/main.cpp
@@ -3,14 +3,13 @@
 #include <zmq.hpp>
 #include <thread>
 
-#include "ros/ros.h"
+#include <ros/ros.h>
 #include "Help.h"
 #include "ActuationAngle.pb.h"
 #include "Node.pb.h"
 #include "Sensors.pb.h"
 #include "sailbot_msg/actuation_angle.h"
-#include "sailbot_msg/windSensor.h"
-#include "sailbot_msg/GPS.h"
+#include "sailbot_msg/Sensors.h"
 
 /*
  * Needed for communication over ethernet
@@ -28,11 +27,7 @@ int send(const std::string &serialized_data) {
  * Needed for communication with other ROS nodes
  */
 ros::Subscriber nt_sub;
-ros::Publisher gps_0_pub;
-ros::Publisher gps_1_pub;
-ros::Publisher wind_sensor_0_pub;
-ros::Publisher wind_sensor_1_pub;
-ros::Publisher wind_sensor_2_pub;
+ros::Publisher sensors_pub;
 
 void ActuationCallBack(const sailbot_msg::actuation_angle ros_actuation_angle) {
     /*
@@ -70,37 +65,48 @@ void PublishSensorData() {
             NetworkTable::Node node;
             node.ParseFromString(message_serialized);
 
-            sailbot_msg::windSensor wind_sensor_0;
-            sailbot_msg::windSensor wind_sensor_1;
-            sailbot_msg::windSensor wind_sensor_2;
-            sailbot_msg::GPS gps_0;
-            sailbot_msg::GPS gps_1;
-            NetworkTable::Sensors proto_sensors = NetworkTable::RootToSensors(&node);
+        NetworkTable::Sensors proto_sensors = NetworkTable::RootToSensors(&node);
+        sailbot_msg::Sensors sensors;
+        sensors.boom_angle_sensor_angle = proto_sensors.boom_angle_sensor().sensor_data().angle();
+        sensors.wind_sensor_0_speed = proto_sensors.wind_sensor_0().iimwv().wind_speed();
+        sensors.wind_sensor_0_direction = proto_sensors.wind_sensor_0().iimwv().wind_direction();
+        sensors.wind_sensor_0_reference = proto_sensors.wind_sensor_0().iimwv().wind_reference();
+        sensors.wind_sensor_1_speed = proto_sensors.wind_sensor_1().iimwv().wind_speed();
+        sensors.wind_sensor_1_direction = proto_sensors.wind_sensor_1().iimwv().wind_direction();
+        sensors.wind_sensor_1_reference = proto_sensors.wind_sensor_1().iimwv().wind_reference();
+        sensors.wind_sensor_2_speed = proto_sensors.wind_sensor_2().iimwv().wind_speed();
+        sensors.wind_sensor_2_direction = proto_sensors.wind_sensor_2().iimwv().wind_direction();
+        sensors.wind_sensor_2_reference = proto_sensors.wind_sensor_2().iimwv().wind_reference();
+        sensors.gps_0_timestamp = proto_sensors.gps_0().gprmc().utc_timestamp();
+        sensors.gps_0_latitude = proto_sensors.gps_0().gprmc().latitude();
+        sensors.gps_0_longitude = proto_sensors.gps_0().gprmc().longitude();
+        sensors.gps_0_latitude_loc = proto_sensors.gps_0().gprmc().latitude_loc();
+        sensors.gps_0_longitude_loc = proto_sensors.gps_0().gprmc().longitude_loc();
+        sensors.gps_0_groundspeed = proto_sensors.gps_0().gprmc().ground_speed();
+        sensors.gps_0_track_made_good = proto_sensors.gps_0().gprmc().track_made_good();
+        sensors.gps_0_magnetic_variation = proto_sensors.gps_0().gprmc().magnetic_variation();
+        sensors.gps_0_magnetic_variation_sense = proto_sensors.gps_0().gprmc().magnetic_variation_sense();
+        sensors.gps_1_timestamp = proto_sensors.gps_1().gprmc().utc_timestamp();
+        sensors.gps_1_latitude = proto_sensors.gps_1().gprmc().latitude();
+        sensors.gps_1_longitude = proto_sensors.gps_1().gprmc().longitude();
+        sensors.gps_1_latitude_loc = proto_sensors.gps_1().gprmc().latitude_loc();
+        sensors.gps_1_longitude_loc = proto_sensors.gps_1().gprmc().longitude_loc();
+        sensors.gps_1_groundspeed = proto_sensors.gps_1().gprmc().ground_speed();
+        sensors.gps_1_track_made_good = proto_sensors.gps_1().gprmc().track_made_good();
+        sensors.gps_1_magnetic_variation = proto_sensors.gps_1().gprmc().magnetic_variation();
+        sensors.gps_1_magnetic_variation_sense = proto_sensors.gps_1().gprmc().magnetic_variation_sense();
+        sensors.accelerometer_x_axis_acceleration = \
+            proto_sensors.accelerometer().boat_orientation_data().x_axis_acceleration();
+        sensors.accelerometer_y_axis_acceleration = \
+        proto_sensors.accelerometer().boat_orientation_data().y_axis_acceleration();
+        sensors.accelerometer_z_axis_acceleration = \
+        proto_sensors.accelerometer().boat_orientation_data().z_axis_acceleration();
 
-            gps_0.lat = proto_sensors.gps_0().gprmc().latitude();
-            gps_0.lon = proto_sensors.gps_0().gprmc().longitude();
-            gps_0.speedKmph = proto_sensors.gps_0().gprmc().ground_speed();
-            gps_1.lat = proto_sensors.gps_1().gprmc().latitude();
-            gps_1.lon = proto_sensors.gps_1().gprmc().longitude();
-            gps_1.speedKmph = proto_sensors.gps_1().gprmc().ground_speed();
-
-            wind_sensor_0.measuredDirectionDegrees = proto_sensors.wind_sensor_0().iimwv().wind_direction();
-            wind_sensor_0.measuredSpeedKmph = proto_sensors.wind_sensor_0().iimwv().wind_speed();
-            wind_sensor_1.measuredDirectionDegrees = proto_sensors.wind_sensor_0().iimwv().wind_direction();
-            wind_sensor_1.measuredSpeedKmph = proto_sensors.wind_sensor_1().iimwv().wind_speed();
-            wind_sensor_2.measuredDirectionDegrees = proto_sensors.wind_sensor_2().iimwv().wind_direction();
-            wind_sensor_2.measuredSpeedKmph = proto_sensors.wind_sensor_2().iimwv().wind_speed();
-
-            // TODO(anyone): still need accelerometer and boom angle sensor
+        sensors_pub.publish(sensors);
 
             std::cout << "Publishing sensor data. ex: wind_sensor_0 speed: " \
-                << wind_sensor_0.measuredSpeedKmph << std::endl;
+                << sensors.wind_sensor_0_speed << std::endl;
 
-            gps_0_pub.publish(gps_0);
-            gps_1_pub.publish(gps_1);
-            wind_sensor_0_pub.publish(wind_sensor_0);
-            wind_sensor_1_pub.publish(wind_sensor_1);
-            wind_sensor_2_pub.publish(wind_sensor_2);
         } else {
             std::cout << "Failed to send ros message" << std::endl;
         }
@@ -136,11 +142,7 @@ int main(int argc, char** argv) {
     }
     std::thread publish_sensor_data(PublishSensorData);
 
-    gps_0_pub = n.advertise<sailbot_msg::GPS>("gps_0", 100);
-    gps_1_pub = n.advertise<sailbot_msg::GPS>("gps_1", 100);
-    wind_sensor_0_pub = n.advertise<sailbot_msg::windSensor>("wind_sensor_0", 100);
-    wind_sensor_1_pub = n.advertise<sailbot_msg::windSensor>("wind_sensor_1", 100);
-    wind_sensor_2_pub = n.advertise<sailbot_msg::windSensor>("wind_sensor_2", 100);
+    sensors_pub = n.advertise<sailbot_msg::Sensors>("sensors", 100);
 
     nt_sub = n.subscribe("/actuation_angles", 100, ActuationCallBack);
     ros::spin();
