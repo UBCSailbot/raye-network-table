e2cf364fc575f74783a19be80ad792062f8e2ff4
jahn18 <john.ahn.es@gmail.com>
2020-08-27 17:31:04 -0700

    NAV-59: Add sensor playback from .csv file
    Add functionality to play back files generated
    by record_sensors.py. Use -f <filename> to
    specify what file to playback. A special flag --loop
    can be used to play back a file forever, looping at the
    end.

--- scripts/mock_sensors.py
+++ scripts/mock_sensors.py
@@ -4,11 +4,49 @@
 # --> needs virtual can set up first -> ./setup_vcan.sh
 # needs server ->  in build/bin -> ./server &
 # You should run this as a background job -> python3 mock_sensors.py -c vcan0 &
+#-------------------------------------------------------------------------------------
+# Script also can playback sensor data recorded from a log file to a virtual CAN bus 
+# **ONLY takes in a csv file created from the record_sensors script**
+# --> Needs virtual can set up first -> ./setup_vcan.sh
+# --> To run script -> python3 mock_sensors.py -c vcan0 -f [file_name]
+# (OPTIONAL) Continuously loop through the data -> python3 mock_sensors.py -c vcan0 -f [file_name] --loop   
+
 import time
 import can
 import sys
 import argparse
+import csv 
 
+def playback_sensor_data(input_file, channel, loop):
+    bus = can.interface.Bus(bustype='socketcan', channel=channel, bitrate=250000)
+    print("Sending CAN messages on {}".format(bus.channel))
+    logfile = open(input_file, "r")
+    # Prevents the first line in the csv file that outlines column names from being scanned
+    next(logfile) 
+    last_line = None
+    while True:
+        reader = csv.reader(logfile)
+        for line in reader:
+            msg = can.Message(arbitration_id=int(line[1]), dlc=None, data=bytes.fromhex(line[2]))
+            # Skip sleeping when reading first line: no previous timestamp to reference off from
+            if last_line is not None:
+                # Calculate the amount of time to sleep for by
+                # getting the different between the current and last
+                # timestamp
+                time.sleep((int(line[0])-int(last_line[0]))/1000)  
+            try:
+                bus.send(msg)
+            except can.CanError:
+                print("Error Sending on CAN bus")
+                pass
+            last_line = line
+        if loop:
+            logfile.seek(0)
+            next(logfile)
+            last_line = None 
+            time.sleep(1)   
+        else:
+            break
 
 def send_sensor_data(device, SID, channel):
     bus = can.interface.Bus(bustype='socketcan', channel=channel, bitrate=250000)
@@ -16,7 +54,7 @@ def send_sensor_data(device, SID, channel):
     print("device = {} \nRTR: 0b0\nDLC: 8\n".format(device))
     if device == 'bms':
         while True:
-            msg = can.Message(arbitration_id=SID[device][i], dlc=0x8, data=[20,20,20,20,20,20,20,20])
+            msg = can.Message(arbitration_id=SID[device][i], dlc=0x8, 		data=[20,20,20,20,20,20,20,20])
             try:
                 bus.send(msg)
             except can.CanError:
@@ -36,7 +74,7 @@ def send_sensor_data(device, SID, channel):
              
     elif device == 'sail':
         while True:
-            # cycles through all of the CAN ID frames for the gps
+            # cycles through all of the CAN ID frames for the gparser.add_argument("-d", "--device_id", help="device to simulate. Options: gps, bms, wind, acc", default='all')ps
             for i in range(len(SID[device])):
                 msg = can.Message(arbitration_id=SID[device][i], dlc=0x8, data=[40,40,40,40,40,40,40,40])
                 try:
@@ -71,6 +109,7 @@ def send_sensor_data(device, SID, channel):
                 except can.CanError:
                     print("Error Sending on CAN bus")
                     pass
+          
 def main():
     # SID is taken from frame_parser.h data relating to the CAN ID of each device
     SID = {'bms': [0x08], 
@@ -81,21 +120,30 @@ def main():
            'all': [0x08, 0x11, 0x100, 0x101, 
                    0x110, 0x0F, 0x0, 0x10, 
                    0x111, 0xAC]}
+
     parser = argparse.ArgumentParser(description="Sensor mocking") 
     parser.add_argument("-d", "--device_id", help="device to simulate. Options: gps, bms, wind, acc", default='all')
-    parser.add_argument("-c", "--channel", help="Input the channel you want to use", default=None)  
+    parser.add_argument("-c", "--channel", help="Input the channel you want to use", default=None) 
+    parser.add_argument("-f", "--file_name", help="Input the sensor data file you wish to mock", default=None)
+    parser.add_argument("--loop", help="Continuously playback the sensor data in a loop", action='store_true')    
     args = parser.parse_args()
     channel = args.channel
+    logfile = args.file_name
+    loop = args.loop 
     
-    if channel == None:
+    if channel is None:
         print("Error: channel not specified")
         print("Example usage: 'python3 mock_sensors.py -c vcan0'")
         return 
 
-    if args.device_id in SID:
+    if logfile:
+        if loop:
+            print("Loop = ON")
+        else:
+            print("Loop = OFF")
+        playback_sensor_data(logfile, channel, loop)
+    else: 
         send_sensor_data(args.device_id, SID, channel)
-    else:
-        print("device not found. Exiting...")
 
     return
 
