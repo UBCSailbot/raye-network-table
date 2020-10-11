bde291ae6035e9939532fa44b15171ddf7e2e8eb
briellelaw <lawbrielle@gmail.com>
2020-10-10 22:24:22 -0700

    NAV-102 Python api to connect to network table
    Added a flag --python-client to network table stress test
    to test the python Connection class instead of c++.
    
    Reorganized python code. The generated code
    is now in src/python/generated_python.
    Projects which use this now have a symbolic
    link to that folder.
    
    Also updated CMakeLists.txt to run sed
    on generated python code to replace
    absolute imports with relative imports.
    See the comment in bottom of
    src/python/CMakeLists.txt for more info

--- scripts/network_table_stress_test.py
+++ scripts/network_table_stress_test.py
@@ -17,8 +17,11 @@ import subprocess
 import sys
 from time import sleep
 from threading import Thread
+from nt_connection.Connection import *
+from generated_python import Value_pb2
 
 continue_server = True
+error_occured = False
 
 
 def run_server():
@@ -49,8 +52,56 @@ def run_server_and_fake_crashes():
         os.killpg(os.getpgid(server.pid), signal.SIGTERM)
         sleep(.01)
 
+    os.killpg(os.getpgid(server.pid), signal.SIGTERM)
 
-if __name__ == "__main__":
+
+def run_python_client():
+    nt_connection = Connection()
+    nt_connection.Connect()
+
+    num_queries = 10
+    for i in range(num_queries):
+        set_value_a = Value_pb2.Value()
+        set_value_a.type = Value_pb2.Value.Type.INT
+        set_value_a.int_data = 11
+        set_value_b = Value_pb2.Value()
+        set_value_b.type = Value_pb2.Value.Type.STRING
+        set_value_b.string_data = "HELLO"
+        try:
+            nt_connection.setValues(
+                    {'/python_client_dummy_data/a': set_value_a,
+                     '/python_client_dummy_data/b': set_value_b})
+        except ConnectionError as e:
+            print(str(e))
+            error_occured = True
+            return
+
+        try:
+            get_node = nt_connection.getNodes(
+                    ['/python_client_dummy_data/a',
+                     '/python_client_dummy_data/b'])
+            get_int_data = get_node['/python_client_dummy_data/a'].value.int_data
+            if not get_int_data == set_value_a.int_data:
+                # Use old style python string formatting, the BBB is still on python 3.5
+                print("expected {}, got back {}".format(set_value_a.int_data, get_int_data))
+                error_occured = True
+                return
+
+            get_string_data = get_node['/python_client_dummy_data/b'].value.string_data
+            if not get_string_data == set_value_b.string_data:
+                print("expected {}, got back {}".format(set_value_b.string_data, get_string_data))
+                error_occured = True
+                return
+
+        except ConnectionError as e:
+            print(str(e))
+            error_occured = True
+            return
+
+    nt_connection.Disconnect()
+
+
+if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Network Table Stress Test")
     parser.add_argument(
         "-n", "--num-clients",
@@ -61,16 +112,22 @@ if __name__ == "__main__":
         "--compile", help="Compile the network table first",
         action='store_true')
     parser.add_argument(
-        "--disable-fake-crashes", help="Stop the network table from crashing/restarting",
+        "--disable-fake-crashes", help="Stop the network table\
+                from crashing/restarting",
         action='store_true')
     parser.add_argument(
         "--clients-only", help="Only run the clients, no server",
+        action='store_true')
+    parser.add_argument(
+        "--python-client", help="Runs the python client\
+                instead of the C++ client",
         action='store_true')
     args = parser.parse_args()
 
     num_clients = args.num_clients
     disable_fake_crashes = args.disable_fake_crashes
     clients_only = args.clients_only
+    python_client = args.python_client
     build = args.compile  # variable name compile is not allowed
 
     # Go to the build directory
@@ -94,31 +151,44 @@ if __name__ == "__main__":
             server_thread = Thread(target=run_server_and_fake_crashes)
         server_thread.start()
 
-    # This is an array of client processes which will communicate with the server.
-    # They will all run at the same time, then the return value of each
-    # one will be checked.
-    clients = [subprocess.Popen(['./bin/client'],
-                                preexec_fn=os.setsid)
-               for i in range(num_clients)]
+    if python_client:
+        clients = [Thread(target=run_python_client)
+                   for i in range(num_clients)]
 
-    errors_occured = 0
-    for client in clients:
-        client.wait()
-        errors_occured += client.returncode
+        for client in clients:
+            client.start()
 
-    if errors_occured == 0:
-        print("No problems detected.")
+        for client in clients:
+            client.join()
+
     else:
-        print("One or more clients failed.")
+        # This is an array of client processes which will communicate with the server.
+        # They will all run at the same time, then the return value of each
+        # one will be checked.
+        clients = [subprocess.Popen(['./bin/client'],
+                                    preexec_fn=os.setsid)
+                   for i in range(num_clients)]
 
-    # Terminal any remaining processes
+        for client in clients:
+            client.wait()
+            if not client.returncode == 0:
+                error_occured = True
+
+        # Terminal any remaining processes
+        for client in clients:
+            try:
+                os.killpg(os.getpgid(client.pid), signal.SIGTERM)
+            except OSError:
+                # The client should have exited normally.
+                pass
+
     if not clients_only:
         continue_server = False
         server_thread.join()
-    for client in clients:
-        try:
-            os.killpg(os.getpgid(client.pid), signal.SIGTERM)
-        except OSError:
-            # The client should have exited normally.
-            pass
-    sys.exit(errors_occured)
+
+    if error_occured:
+        print("One or more clients failed.")
+        sys.exit(-1)
+    else:
+        print("No problems detected.")
+        sys.exit(0)
