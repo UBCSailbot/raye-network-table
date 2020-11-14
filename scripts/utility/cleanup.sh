bf921d451e04d7e858094837e00ac0c5fd8e3fc8
Sadwyth <meloncxllie@gmail.com>
2020-11-14 13:36:03 -0800

    Added cleanup.sh Use this to kill any leftover processes

---
+++ scripts/utility/cleanup.sh
@@ -0,0 +1,13 @@
+pkill -f network_table_server
+pkill -f bbb_eth_listener
+pkill -f bbb_canbus_listener
+pkill -f bbb_satellite_listener
+pkill -f Iridium9602
+pkill -f mock_sensors
+pkill -f socat
+rm -r /tmp/can_bbb_nuc_integration/
+pkill -f roscore
+pkill -f nuc_eth_listener
+pkill -f raye_communication_node
+pkill -f network_table_server
+pkill -f land_satellite_listener
