cb7363f8233cfcf2e5faf94c4a5c725f58b11def
alexmac22347 <alexmac22347@protonmail.com>
2020-11-19 11:24:52 -0800

    NAV-112
    Complete local pathfinding integration.
    Local pathfinding has been added to the Jenkins test. Right now
    it just sits there and does nothing, we'll have to make it do something
    later, ie give it real data.
    
    Also updated cleanup.sh script to kill local pathfinding.

--- scripts/utility/cleanup.sh
+++ scripts/utility/cleanup.sh
@@ -1,3 +1,4 @@
+# bbb
 pkill -f network_table_server
 pkill -f bbb_eth_listener
 pkill -f bbb_canbus_listener
@@ -6,8 +7,11 @@ pkill -f Iridium9602
 pkill -f mock_sensors
 pkill -f socat
 rm -r /tmp/can_bbb_nuc_integration/
+
+# nuc
 pkill -f roscore
 pkill -f nuc_eth_listener
 pkill -f raye_communication_node
+pkill -f main_loop.py
 pkill -f network_table_server
 pkill -f land_satellite_listener
