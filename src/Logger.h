2fb8848e0e139c6156ea419826a2f8df870c6253
rickcheng1337 <yuedong.cheng@hotmail.com>
2021-03-10 14:29:53 -0700

    Added class properties and functions in Logger.h

--- src/Logger.h
+++ src/Logger.h
@@ -85,5 +85,48 @@ class Logger {
 };
 }
 
+#include <Exceptions.h>
+#include <Connection.h>
+
+namespace NetworkTable {
+    class Logger {
+    public:
+        // Constructor
+        Logger(bool text_logging);
+
+        /*
+         * Different levels of logging severity
+         * 
+         * @param msg - error message passed in by caller
+        */
+        void Trace(std::string msg); // Trace log
+        void Debug(std::string msg); // Debug log
+        void Info(std::string msg); // Info log
+        void Warning(std::string msg); // Warning log
+        void Error(std::string msg); // Error log
+        void Fatal(std::string msg); // Fatal log
+
+        /*
+         * Setter for Network Table connection
+         * 
+         * @param connection 
+        */
+        void SetNTConn(NetworkTable::Connection conn);
+
+    private:
+        // Init function to set up boost log paramters
+        void Init();
+
+        /*
+        * Generates a protobuf error object
+        */
+        NetworkTable::Value GenPBErr(std::string msg, int severity);
+
+        NetworkTable::Connection nt_conn;
+        std::string file_path;
+
+    };
+}
+
 
 #endif  // LOGGER_H_
