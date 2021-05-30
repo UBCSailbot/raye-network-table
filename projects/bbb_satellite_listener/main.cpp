13c4f84a54cd14ce9a051977712723a03a767972
briellelaw <lawbrielle@gmail.com>
2021-05-30 03:22:39 -0700

    NAV-201 Decode waypoint data from rockblock

--- projects/bbb_satellite_listener/main.cpp
+++ projects/bbb_satellite_listener/main.cpp
@@ -10,6 +10,9 @@
 #include <boost/algorithm/string.hpp>
 #include <mutex>
 #include <stdexcept>
+#include <vector>
+#include <sstream>
+#include <cstdio>
 
 #include "Connection.h"
 #include "Help.h"
@@ -18,6 +21,7 @@
 #include "Satellite.pb.h"
 #include "Value.pb.h"
 #include "Exceptions.h"
+#include "Uri.h"
 
 // Stores serialized sensor and uccm data to send to rockblock
 std::string latest_sensors_satellite_string;  // NOLINT(runtime/string)
@@ -56,6 +60,16 @@ std::string readLine(boost::asio::serial_port &p) {  // NOLINT(runtime/reference
                 result += c;
         }
     }
+}
+
+std::vector<char> HexToBytes(const std::string& hex) {
+  std::vector<char> bytes;
+  for (unsigned int i = 0; i < hex.length(); i += 2) {
+    std::string byteString = hex.substr(i, 2);
+    char byte = static_cast<char>(strtol(byteString.c_str(), NULL, 16));
+    bytes.push_back(byte);
+  }
+  return bytes;
 }
 
 void send(const std::string &data) {
@@ -103,26 +117,20 @@ void send(const std::string &data) {
     std::cout << readLine(serial) << std::endl;
 }
 
-/* Deserialize google protobuf message */
-std::string decodeMessage(boost::asio::serial_port &p /* NOLINT(runtime/references) */) {
+std::string decode_message(std::string message) {
     NetworkTable::Satellite satellite;
-    std::string data;
-    char c;
-
-    for (unsigned int i = 0; i < receive_size; i++) {
-        boost::asio::read(p, boost::asio::buffer(&c, 1));
-        data += c;
+    char hex_message[receive_size];  // NOLINT(runtime/arrays)
+    int i = 0;
+    for (const auto &item : message) {
+        std::sprintf(&hex_message[i*2], "%02x", static_cast<int>(item));  // NOLINT(runtime/printf)
+        i++;
     }
 
-    satellite.ParseFromString(data);
+    std::vector<char> byte_message = HexToBytes(std::string(hex_message));
+    std::string str_data(byte_message.begin(), byte_message.end());
+    satellite.ParseFromString(str_data);
 
-    if (satellite.type() == NetworkTable::Satellite::SENSORS) {
-        std::cout << "SENSOR DATA" << std::endl;
-        return satellite.DebugString();
-    } else if (satellite.type() == NetworkTable::Satellite::UCCMS) {
-        std::cout << "UCCM DATA" << std::endl;
-        return satellite.DebugString();
-    } else if (satellite.type() == NetworkTable::Satellite::VALUE &&
+    if (satellite.type() == NetworkTable::Satellite::VALUE &&
                satellite.value().type() == NetworkTable::Value::WAYPOINTS) {
         std::cout << "WAYPOINT DATA" << std::endl;
         connection.SetValue("waypoints", satellite.value());
@@ -133,27 +141,30 @@ std::string decodeMessage(boost::asio::serial_port &p /* NOLINT(runtime/referenc
 }
 
 std::string receive_message(const std::string &status) {
-    std::string response;
+    std::string message;
+
     if (status == "1") {
         // Command to receive binary MT message
         std::string msg = "AT+SBDRB\r";
         boost::asio::write(serial, boost::asio::buffer(msg.c_str(), msg.size()));
-        std::cout << readLine(serial) << std::endl;
+
+        // Response includes embedded payload
+        // AT+SBDRB__<str_payload>
+        std::string response = readLine(serial);
+        std::string str_payload = response.substr(10);
         std::cout << readLine(serial) << std::endl;
 
-        response = decodeMessage(serial);
-        std::cout << readLine(serial) << std::endl;
-        std::cout << readLine(serial) << std::endl;
-        std::cout << readLine(serial) << std::endl;
+        message = decode_message(str_payload);
+
     } else if (status == "2") {
-        response = "Error checking mailbox";
+        message = "Error checking mailbox";
     } else if (status == "0") {
         std::string null_msg = "Mailbox empty";
-        response = null_msg;
+        message  = null_msg;
     } else {
-        response = "Non-valid status";
+        message  = "Non-valid status";
     }
-    return response;
+    return message;
 }
 
 /* Check for and receive MT messages in queue */
@@ -161,7 +172,7 @@ void receive() {
     std::lock_guard<std::mutex> lck(serialPort_mtx);
     std::cout << "Receiving Data" << std::endl;
 
-    // Mailbox check (checks for messages in the queue)
+    // Initiate an Extended SBD Session
     std::string sbdInit("AT+SBDIX\r");
     boost::asio::write(serial, boost::asio::buffer(sbdInit.c_str(), sbdInit.size()));
 
@@ -199,25 +210,32 @@ void RootCallback(NetworkTable::Node node, \
     const std::map<std::string, NetworkTable::Value> &diffs, \
     bool is_self_reply) {
 
-    // Store updated network table data
-    NetworkTable::Satellite sensors_satellite;
-    NetworkTable::Satellite uccms_satellite;
-    sensors_satellite.set_type(NetworkTable::Satellite::SENSORS);
-    uccms_satellite.set_type(NetworkTable::Satellite::UCCMS);
+    for (const auto& uris : diffs) {
+        std::string uri = uris.first;
+        std::cout << uri << std::endl;
+        // We do Not want to send back waypoint data that was just received
+        if (uri != WAYPOINTS_GP) {
+            // Store updated network table data
+            NetworkTable::Satellite sensors_satellite;
+            NetworkTable::Satellite uccms_satellite;
+            sensors_satellite.set_type(NetworkTable::Satellite::SENSORS);
+            uccms_satellite.set_type(NetworkTable::Satellite::UCCMS);
 
-    NetworkTable::Sensors sensors = NetworkTable::RootToSensors(&node);
-    NetworkTable::Uccms uccms = NetworkTable::RootToUccms(&node);
+            NetworkTable::Sensors sensors = NetworkTable::RootToSensors(&node);
+            NetworkTable::Uccms uccms = NetworkTable::RootToUccms(&node);
 
-    // TODO(alex): I don't think this is a memory leak,
-    // but should test with valgrind
-    // This creates an extra object (one on the stack and one on the heap)
-    // but it shouldnt matter much. The stack one is going to get deallocated
-    // pretty soon
-    sensors_satellite.set_allocated_sensors(new NetworkTable::Sensors(sensors));
-    uccms_satellite.set_allocated_uccms(new NetworkTable::Uccms(uccms));
+            // TODO(alex): I don't think this is a memory leak,
+            // but should test with valgrind
+            // This creates an extra object (one on the stack and one on the heap)
+            // but it shouldnt matter much. The stack one is going to get deallocated
+            // pretty soon
+            sensors_satellite.set_allocated_sensors(new NetworkTable::Sensors(sensors));
+            uccms_satellite.set_allocated_uccms(new NetworkTable::Uccms(uccms));
 
-    sensors_satellite.SerializeToString(&latest_sensors_satellite_string);
-    uccms_satellite.SerializeToString(&latest_uccms_satellite_string);
+            sensors_satellite.SerializeToString(&latest_sensors_satellite_string);
+            uccms_satellite.SerializeToString(&latest_uccms_satellite_string);
+        }
+    }
 }
 
 /* Send most recent sensor data */
