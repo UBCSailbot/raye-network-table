d20dd22eccc5b5cf5cdd1ce372ef018635e10dec
vladakozachok <spam4318@gmail.com>
2021-06-05 11:43:13 -0700

    serial_api file created

---
+++ src/python/serial_api.py
@@ -0,0 +1,55 @@
+import serial
+import argparse
+
+def flushBuffer(ser):
+    ser.flushInput()
+    ser.flushOutput()
+
+def writeBuffer(ser, message):
+    ser.write(message)
+
+def readBuffer(ser):
+    ser.readline() 
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Working with the serial port.")
+
+    parser.add_argument('-f',
+                        '--functionality',
+                        metavar='FUNCTIONALITY',
+                        type=str,
+                        help='desired functionality',
+                        choices = ['FLUSH', 'WRITE', 'READ'],
+                        required=True)
+
+    parser.add_argument('-s',
+                        '--serial',
+                        metavar='SERIAL',
+                        type=str,
+                        help='serial device',
+                        required=True)
+
+    parser.add_argument('-m',
+                        '--message',
+                        metavar='MESSAGE',
+                        type=str,
+                        help='message',
+                        required=False)
+
+    args = parser.parse_args()
+    function = args.functionality
+    device = args.serial
+    message = args.message
+
+    ser = serial.Serial(device)
+
+    if function == 'FLUSH':
+        flushBuffer(ser)
+    elif function == 'WRITE':
+        writeBuffer(ser, message)
+    elif function == 'READ':
+        readBuffer(ser) 
+
+
+
+
