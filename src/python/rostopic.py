ec41a32cc36046ad6a7969c7d7ef56e759d891ec
briellelaw <lawbrielle@gmail.com>
2021-06-12 15:06:23 -0700

    NAV-192-Intermediate update rostopic references to match sailbot-msg

--- src/python/rostopic.py
+++ src/python/rostopic.py
@@ -5,30 +5,38 @@ be reassigned in the scope of that specific project.
 
 """
 
+WIND1_SPEED_TOPIC = 'wind_sensor_1_speed_knots'
+WIND1_ANGLE_TOPIC = 'wind_sensor_1_angle_degrees'
+WIND2_SPEED_TOPIC = 'wind_sensor_2_speed_knots'
+WIND2_ANGLE_TOPIC = 'wind_sensor_2_angle_degrees'
+WIND3_SPEED_TOPIC = 'wind_sensor_3_speed_knots'
+WIND3_ANGLE_TOPIC = 'wind_sensor_3_angle_degrees'
 
-WIND1_SPEED_TOPIC = 'wind_sensor_0_speed'
-WIND1_ANGLE_TOPIC = 'wind_sensor_0_direction'
-WIND2_SPEED_TOPIC = 'wind_sensor_1_speed'
-WIND2_ANGLE_TOPIC = 'wind_sensor_1_direction'
-WIND3_SPEED_TOPIC = 'wind_sensor_2_speed'
-WIND3_ANGLE_TOPIC = 'wind_sensor_2_direction'
+GPS_CAN_TIME_TOPIC = 'gps_can_timestamp_utc'
+GPS_CAN_LAT_TOPIC = 'gps_can_latitude_degreeMinutes'
+GPS_CAN_LON_TOPIC = 'gps_can_longitude_degreeMinutes'
+GPS_CAN_GNDSPEED_TOPIC = 'gps_can_groundspeed_knots'
+GPS_CAN_TMG_TOPIC = 'gps_can_track_made_good_degrees'
+GPS_CAN_MAGVAR_TOPIC = 'gps_can_magnetic_variation_degrees'
+GPS_CAN_TRUE_HEADING_TOPIC = 'gps_can_true_heading_degrees'
 
-GPS_CAN_TIME_TOPIC = 'gps_0_timestamp'
-GPS_CAN_LAT_TOPIC = 'gps_0_latitude'
-GPS_CAN_LON_TOPIC = 'gps_0_longitude'
-GPS_CAN_GNDSPEED_TOPIC = 'gps_0_ground_speed'
-GPS_CAN_TMG_TOPIC = 'gps_0_track_made_good'
-GPS_CAN_MAGVAR_TOPIC = 'gps_0_magnetic_variation'
-GPS_CAN_TRUE_HEADING_TOPIC = 'gps_0_true_heading'
+GPS_AIS_TIME_TOPIC = 'gps_ais_timestamp_utc'
+GPS_AIS_LAT_TOPIC = 'gps_ais_latitude_degreeMinutes'
+GPS_AIS_LON_TOPIC = 'gps_ais_longitude_degreeMinutes'
+GPS_AIS_GNDSPEED_TOPIC = 'gps_ais_groundspeed_knots'
+GPS_AIS_TMG_TOPIC = 'gps_ais_track_made_good_degrees'
+GPS_AIS_MAGVAR_TOPIC = 'gps_ais_magnetic_variation_degrees'
+GPS_AIS_TRUE_HEADING_TOPIC = 'gps_ais_true_heading_degrees'
 
-GPS_AIS_TIME_TOPIC = 'gps_1_timestamp'
-GPS_AIS_LAT_TOPIC = 'gps_1_latitude'
-GPS_AIS_LON_TOPIC = 'gps_1_longitude'
-GPS_AIS_GNDSPEED_TOPIC = 'gps_1_ground_speed'
-GPS_AIS_TMG_TOPIC = 'gps_1_track_made_good'
-GPS_AIS_MAGVAR_TOPIC = 'gps_1_magnetic_variation'
-GPS_AIS_TRUE_HEADING_TOPIC = 'gps_1_true_heading'
+WINCH_MAIN_TOPIC = 'winch_main_angle_degrees'
+WINCH_JIB_TOPIC = 'winch_jib_angle_degrees'
+RUDDER_PORT_TOPIC = 'rudder_port_angle_degrees'
+RUDDER_STBD_TOPIC = 'rudder_stbd_angle_degrees'
 
-ACCELEROMETER_X_TOPIC = "accelerometer_x_axis_acceleration"
-ACCELEROMETER_Y_TOPIC = "accelerometer_y_axis_acceleration"
-ACCELEROMETER_Z_TOPIC = "accelerometer_z_axis_acceleration"
+ACCELEROMETER_X_TOPIC = 'accelerometer_x_force_millig'
+ACCELEROMETER_Y_TOPIC = 'accelerometer_y_force_millig'
+ACCELEROMETER_Z_TOPIC = 'accelerometer_z_force_millig'
+
+GYROSCOPE_X_TOPIC = 'gyroscope_x_velocity_millidegreesps'
+GYROSCOPE_Y_TOPIC = 'gyroscope_y_velocity_millidegreesps'
+GYROSCOPE_Z_TOPIC = 'gyroscope_z_velocity_millidegreesps'
