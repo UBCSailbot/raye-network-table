a4a3de31ad1fc3b2f40a1bd95c92ef85ecdf1cad
briellelaw <lawbrielle@gmail.com>
2021-07-24 14:03:13 -0700

    Script to configure BBB pins

---
+++ scripts/config_bbb_pins.sh
@@ -0,0 +1,10 @@
+# Configure UART pins
+config-pin p9.11 uart
+config-pin p9.13 uart
+config-pin p9.21 uart
+config-pin p9.22 uart
+
+# Configure CAN pins
+config-pin p9.19 can
+config-pin p9.20 can
+
