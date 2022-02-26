96765ad64ba29ab6dc8c823b9f83a2b09ee506cb
Henry Huang <69825683+hhenry01@users.noreply.github.com>
2022-02-26 15:35:37 -0800

    Nav 251 (#133)
    Controls logging script for testing. Will need to be cleaned up later, merging now for testing other components.

---
+++ scripts/ctrl_data_logging/ctrl_data_logging.sh
@@ -0,0 +1,40 @@
+# !/bin/bash
+
+# Logs the relevant rostopics on the NUC for
+# CTRL testing.
+
+
+SENSORS="/sensors"
+HEAD="/desired_heading_degrees"
+ACTUATE_ANG="/rudder_winch_actuation_angle"
+
+ROS_ECHO="rostopic echo -n 1"
+
+OUTPUT_SENSORS="sensor_data.log"
+OUTPUT_HEAD="head_data.log"
+OUTPUT_ACT="act_data.log"
+
+ECHO_DATE="date >> $OUTPUT_SENSORS && date >> $OUTPUT_HEAD && date >> $OUTPUT_ACT"
+# Go to the real directory and not a symlink
+# directory
+cd "$(dirname "$(realpath "$0")")"
+
+printf "\n Starting data logging for CTRL\n"
+printf "\n================================\n"
+
+# Source the ROS topocs
+#../../build/devel/setup.sh
+# Clear the output file
+eval ": > $OUTPUT_SENSORS"
+eval ": > $OUTPUT_HEAD"
+eval ": > $OUTPUT_ACT"
+printf "Saving output to %s\n" $PWD
+
+while :
+do
+  eval "$ECHO_DATE"
+  eval "$ROS_ECHO $SENSORS >> $OUTPUT_SENSORS"
+  eval "$ROS_ECHO $HEAD >> $OUTPUT_HEAD"
+  eval "$ROS_ECHO $ACTUATE_ANG >> $OUTPUT_ACT"
+  sleep 1
+done
