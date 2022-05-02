bb462d5458ad5284c71d6604ef4cca6304afea72
hhenry01 <henryhuang2001@gmail.com>
2022-05-01 18:01:11 -0700

    Accounted for unreliability of satellite mailbox.
    When attempting to check the mailbox, if an error status is returned
    then continue looping to check for waypoints.

--- projects/bbb_satellite_listener/main.cpp
+++ projects/bbb_satellite_listener/main.cpp
@@ -234,9 +234,9 @@ std::string decode_message(const std::string &message) {
  *  @param status 1 if mailbox check was successful
  *                2 if mailbox check failed
  *                0 if mailbox is empty 
- *
+ *  Returns false on error
  */
-std::string receive_message(const std::string &status) {
+bool receive_message(const std::string &status) {
     std::string message;
 
     if (status == "1") {
@@ -252,16 +252,18 @@ std::string receive_message(const std::string &status) {
 
         // Retrieve the decoded waypoint data
         message = decode_message(str_payload);
-
+        std::cout << message << std::endl;
+        return true;
     } else if (status == "2") {
-        message = "Error checking mailbox";
+        std::cout << "Error checking mailbox" << std::endl;
+        return false;
     } else if (status == "0") {
-        std::string null_msg = "Mailbox empty";
-        message  = null_msg;
+        std::cout << "Mailbox empty" << std::endl;
+        return true;
     } else {
-        message  = "Non-valid status";
+        std::cout << "Non-valid status" << std::endl;
+        return false;
     }
-    return message;
 }
 
 /**
@@ -272,7 +274,7 @@ void receive() {
     std::cout << "Receiving Data" << std::endl;
 
     bool queueEmpty = true;
-    int queueSize;
+    int queueSize = 0;
 
     do {
         // Initiate an Extended SBD Session
@@ -294,19 +296,17 @@ void receive() {
         // Extract the mailbox status and queue size
         receive_size = std::stoul(response_split[4], NULL, 0);
         std::string status(response_split[2]);
-        queueSize = std::stoi(response_split[5], NULL, 0);
-        if (queueSize > 0)
-            queueEmpty = false;
-
         std::cout << readLine(serial) << std::endl;
         std::cout << readLine(serial) << std::endl;
-
         // Read the expected waypoints off the seral port
-        std::string received_message = receive_message(status);
-        std::cout << received_message <<std::endl;
+        if (receive_message(status)) {
+            queueSize = std::stoi(response_split[5], NULL, 0);
+            if (queueSize > 0)
+                queueEmpty = false;
+        }
     } while (queueSize > 0);
 
-    // Only set waypoints if we actually ahd contents from the queue
+    // Only set waypoints if we actually have contents from the queue
     if (!queueEmpty)
         set_waypoints();
 }
